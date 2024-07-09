9e037ad387bf713c2b2489c652872a0258f4d1dd
Filipe Salviano <filipesalviano@proton.me>
2024-07-09 17:00:39 -0300

    First commit and initial structure

---
+++ WebApiBase.API/Data/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiBase.Models;
+
+namespace WebApiBase.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<EmployeeModel> Employees { get; private set; }
+}
---
+++ WebApiBase.API/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiBase.Enums;
+
+namespace WebApiBase.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DepartmentEnum Department { get; set; }
+        public bool Active { get; set; }
+        public string Shift { get; set; }
+        public DateTime DateCreation { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime UpdateDate { get; set; } = DateTime.Now.ToLocalTime();
+    }
+}
