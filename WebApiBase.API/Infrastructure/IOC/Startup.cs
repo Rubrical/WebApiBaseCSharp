f84d0d14569d7d5e80bd5dbbda05ac1997b0a6c5
Filipe Salviano <filipesalviano@proton.me>
2024-08-02 21:07:27 -0300

    Refactoring actual program to use both startup and program

---
+++ WebApiBase.API/Infrastructure/IOC/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using WebApiBase.Data;
+using WebApiBase.Handlers;
+using WebApiBase.Services.EmployeeService;
+
+namespace WebApiBase.Infrastructure.IOC;
+
+public class Startup(IConfiguration configuration)
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddProblemDetails();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo{Title = "Web Api Base", Version = "v1"});
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath, true);
+        });
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        app.UseExceptionHandler();
+    }
+
+    public void RegisterServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+        
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+    }
+}
--- WebApiBase.API/Program.cs
+++ WebApiBase.API/Program.cs
@@ -1,47 +1,15 @@
-using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using WebApiBase.Data;
-using WebApiBase.Handlers;
-using WebApiBase.Infrastructure;
-using WebApiBase.Services.EmployeeService;
+using WebApiBase.Infrastructure.IOC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo{Title = "Web Api Base", Version = "v1"});
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
-});
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+var startup = new Startup(builder.Configuration);
 
-// services subscribe
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-
-var app = builder.Build();
+startup.ConfigureServices(builder.Services);
+startup.RegisterServices(builder.Services, builder.Configuration);
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+var app = builder.Build();
 
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.UseExceptionHandler();
-
+startup.Configure(app, app.Environment);
 app.Run();
